423dcfe3d02885f18b8b0ad2d089e4fe2e80112f
Priyanshu Lakra <139461877+PriyanshuLakra@users.noreply.github.com>
2024-07-25 16:01:17 +0530

    Update NewSignin.tsx

--- packages/ui/src/NewSignin.tsx
+++ packages/ui/src/NewSignin.tsx
@@ -53,7 +53,7 @@ export const NewSignin = ({ type }: { type: "signup" | "signin" }) => {
                                 <label className="block mb-2 text-sm font-semibold text-black">Phone</label>
                                 <input onChange={(e)=>{
                                     setPhone(e.target.value)
-                                }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="priyanshu Lakra" required />
+                                }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
                             </div>
                         </div>
                         <div className="pt-2">
