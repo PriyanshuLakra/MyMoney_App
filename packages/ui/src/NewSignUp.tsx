4c81abf256aad1492e45a4e4c7cd3ce30e185f88
Priyanshu Lakra <139461877+PriyanshuLakra@users.noreply.github.com>
2024-07-25 16:08:18 +0530

    Update NewSignUp.tsx

--- packages/ui/src/NewSignUp.tsx
+++ packages/ui/src/NewSignUp.tsx
@@ -68,7 +68,7 @@ export const NewSignUp=()=>{
                                 <label className="block mb-2 text-sm font-semibold text-black">Name</label>
                                 <input onChange={(e)=>{
                                     setName(e.target.value)
-                                }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="priyanshu Lakra" required />
+                                }} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
                             </div>
                         </div>
                         
